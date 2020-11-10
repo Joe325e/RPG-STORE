74a3dadb30373e1a34121382fc0fefb5dfcdb7f2
Josiah <Josiah.richarson50@gmail.com>
2020-11-10 12:29:27 -0800

    i put in variables in the fish class

--- Fish.cs
+++ Fish.cs
@@ -8,7 +8,28 @@ namespace Rpg_store
     {
 
        
+        private int itemId;
+        private string itemType;
+        private string itemName;
+        private int itemCost;
 
+        public int itemId { get => itemId; set => itemId = value; }
+        public int itemType { get => itemType; set => itemType = value; }
+        public int itemName { get => itemName; set => itemName = value; }
+        public int itemCost { get => itemCost; set => itemCost = value; }
+       
+
+
+
+        public override void PrintDetails()
+
+        {
+
+            base.PrintDetails();
+
+            Util.Prompt($"={itemId}, itemType={itemType}, itemName={itemName}, itemCost{itemCost}");
+
+        }
 
 
 
